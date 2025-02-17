0735a998b984b0e9d3f02b6160e2ef11a89b3adf
jadisson8 <jadissonap@gmail.com>
2025-02-17 12:15:24 -0300

    static

--- envDjango/api_geek/api_geek/settings.py
+++ envDjango/api_geek/api_geek/settings.py
@@ -152,10 +152,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Rest-Framework
 
