6f8ee66edb668f529297a248221de47125d4d06d
jadisson8 <jadissonap@gmail.com>
2025-02-17 12:22:19 -0300

    remove static_root e estorage whitenoise

--- envDjango/api_geek/api_geek/settings.py
+++ envDjango/api_geek/api_geek/settings.py
@@ -65,7 +65,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'api_geek.urls'
@@ -152,7 +151,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Rest-Framework
 
