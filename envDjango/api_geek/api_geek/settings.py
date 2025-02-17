11dba7278287d0058d5bdbf25e0dcab96e21cfcf
jadisson8 <jadissonap@gmail.com>
2025-02-17 12:01:02 -0300

    MIDDLEWARE estatic

--- envDjango/api_geek/api_geek/settings.py
+++ envDjango/api_geek/api_geek/settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'api_geek.urls'
@@ -151,6 +152,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Rest-Framework
 
